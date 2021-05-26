cce2677bcd68cee2b4fff83ce853d92df150c650
ilmar <khoseilmar@gmail.com>
2021-05-26 10:24:09 -0300

    teste framework

---
+++ src/test_framework.py
@@ -0,0 +1,6 @@
+from tests.M1_test import run
+from ZDT1 import ZDT1
+
+problem = ZDT1(numberOfDecisionVariables=10)
+
+run(10, problem, "tests/zdt1_front.txt", print_igd=True, print_y=False, print_x=False, show_graph=True)
