8461020ded9421346396e19041f509ac049f9632
deadlyshoes <khoseilmar@gmail.com>
2021-10-12 13:20:54 -0300

    Atualizar framework test

--- run_framework.py
+++ run_framework.py
@@ -1,10 +1,10 @@
-from run import run
+from run_keras import run
 
-run("M6", "DTLZ1", "MRGA", "SBX", "Polynomial", "Binary", "CrowdingDistance",
+run("M1", "NN_MNIST", "NSGAII", "SBX", "Polynomial", "Binary", "CrowdingDistance",
     1, 
-    framework_args_file="args_samples/framework_args_M6_R3.json", 
-    moea_args_file="args_samples/MEMO_args.json", 
+    framework_args_file="resources/args_samples/framework_args_M1.json", 
+    moea_args_file="resources/args_samples/MEMO_args.json", 
     pareto_front_file="tests/dtlz1_front.txt", 
-    problem_args_file="args_samples/problem_args_DTLZ.json",
-    mutation_args_file="args_samples/Polynomial_args.json",
-    crossover_args_file="args_samples/SBX_args.json")
+    problem_args_file="resources/args_samples/problem_args_DTLZ.json",
+    mutation_args_file="resources/args_samples/Polynomial_args.json",
+    crossover_args_file="resources/args_samples/SBX_args.json")
