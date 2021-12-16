42ecf40c81c93d86fb04fb76f8fefb09a32dff7c
ilmar <khoseilmar@gmail.com>
2021-12-16 13:12:26 -0300

    adicionar arquivo de teste do M1 com GA otimizando hiperparâmetros

---
+++ run_framework_GA.py
@@ -0,0 +1,10 @@
+from run_hyper import run
+
+run("M1", "SVM_hyperparameters", "GA", "SBX", "Polynomial", "Binary", "CrowdingDistance",
+    1, 
+    framework_args_file="resources/args_samples/framework_args_M1.json",
+    moea_args_file="resources/args_samples/MEMO_args.json",
+    pareto_front_file="tests/zdt1_front.txt",
+    problem_args_file="resources/args_samples/problem_args_ZDT.json",
+    mutation_args_file="resources/args_samples/Polynomial_args.json",
+    crossover_args_file="resources/args_samples/SBX_args.json")
