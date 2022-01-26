3589f47dde74bda8b17e325fcca3ada3fc46509d
deadlyshoes <khoseilmar@gmail.com>
2022-01-26 12:20:55 -0300

    fix MARSAOP

--- src/frameworks/MARSAOP.py
+++ src/frameworks/MARSAOP.py
@@ -52,13 +52,18 @@ class MARSAOP():
         do_change = random_probs < probability
 
         without_change = np.sum(do_change, axis=1) == 0
+        # FIXME: is this iteration necessary?
+        without_change_ids = np.arange(stop=self.m)[without_change]
         selected_variables = np.random.randint(0, self.d, self.m)
-        do_change[without_change, selected_variables] = True
+        for id in without_change_ids:
+            do_change[id][selected_variables[id]] = True
 
         change = np.random.normal(0, self.variance * self.variance, (self.m, self.d))
 
         X = np.tile(bestVariables, (self.m, 1))
         X[do_change] += change[do_change]
+        # repair X
+        np.clip(X, self.problem.decisionVariablesLimit[0], self.problem.decisionVariablesLimit[1], X)
 
         candidate_points = Population(self.problem.numberOfObjectives, self.problem.numberOfDecisionVariables)
         candidate_points.decisionVariables = X
@@ -74,20 +79,20 @@ class MARSAOP():
         Rn_min = np.min(Rn_z)
         Rn_max = np.max(Rn_z)
 
-        V = np.zeros(candidates.shape[0])
+        V = np.zeros(X.shape[0])
         can_div = Rn_min != Rn_max
         V[~can_div] = 1
-        V[can_div] = (Rn_z[can_div] - Rn_min[can_div]) / (Rn_max[can_div] - Rn_min[can_div])
+        V[can_div] = (Rn_z[can_div] - Rn_min) / (Rn_max - Rn_min)
 
         all_dis = scipy.spatial.distance.cdist(X, D.decisionVariables)
         dis = np.min(all_dis, axis=1)
         dis_min = np.min(dis)
         dis_max = np.max(dis)
 
-        W = np.zeros(candidates.shape[0])
+        W = np.zeros(candidates.decisionVariables.shape[0])
         can_div = dis_min != dis_max
         W[~can_div] = 1
-        W[can_div] = (dis_max[can_div] - dis[can_div]) / (dis_max - dis_min)
+        W[can_div] = (-dis[can_div] + dis_max) / (dis_max - dis_min)
 
         S = self.wv * V + self.ww * W
         return X[np.argmin(S)]
@@ -108,10 +113,12 @@ class MARSAOP():
         self.prob = min(20 / self.d, 1)
         assert(self.problem.numberOfObjectives == 1)
 
+        bestPrecision = None
         Pk = genPopulation(self.problem, self.n)
         paretoFront = ParetoFront()
 
         self.problem.evaluate(Pk)
+        print("scores:", Pk.objectives)
 
         for iterator in range(self.Gmax):
             Fkm = np.transpose(Pk.objectives)
@@ -130,8 +137,17 @@ class MARSAOP():
 
             candidate_points = self.generate_candidate_points(cur_prob, bestVariables)
             promising_point = self.select_evaluation_point(candidate_points, SMs[0], Pk)
-            Pk.decisionVariables = np.concatenate(Pk.decisionVariables, np.array([promising_point]))
+            Pk.decisionVariables = np.concatenate((Pk.decisionVariables, np.array([promising_point])))
             self.problem.evaluate(Pk)
+
+            if bestPrecision == None or Pk.objectives[-1][0] < bestPrecision:
+                bestPrecision = Pk.objectives[-1][0]
+                self.T1 += 1
+                self.T2 = 0
+            else:
+                self.T2 += 1
+                self.T1 = 0
+
             self.n += 1
             self.update_variance()
 
