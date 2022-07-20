4472276fb62b0094dd2c6b53334779edeaaea55b
deadlyshoes <khoseilmar@gmail.com>
2022-07-20 09:00:25 -0300

    adicionar dropout em CNN

--- src/problems/keras_cnn.py
+++ src/problems/keras_cnn.py
@@ -3,7 +3,7 @@ from src.Solution import Solution
 
 import keras
 from keras.models import Sequential
-from keras.layers import Conv2D, AveragePooling2D, MaxPooling2D, Dense, Flatten
+from keras.layers import Conv2D, AveragePooling2D, MaxPooling2D, Dense, Flatten, Dropout
 from tensorflow.keras.optimizers import Adam, SGD
 from keras.datasets import mnist
 from keras.utils import np_utils
@@ -14,8 +14,8 @@ import numpy as np
 
 class Keras_CNN(Problem):
     def __init__(self, X_train, y_train, X_test, y_test, input_dim, n_classes):
-        lowerBounds = [0.0 for _ in range(13)]
-        upperBounds = [1.0 for _ in range(13)]
+        lowerBounds = [0.0 for _ in range(15)]
+        upperBounds = [1.0 for _ in range(15)]
 
         # kernel size 1
         lowerBounds[0] = 0.1
@@ -51,11 +51,11 @@ class Keras_CNN(Problem):
 
         # neurons 1
         lowerBounds[8] = 0.01
-        upperBounds[8] = 0.3
+        upperBounds[8] = 0.8
 
         # neurons 2
         lowerBounds[9] = 0.01
-        upperBounds[9] = 0.3
+        upperBounds[9] = 0.8
 
         # activation full 1
         lowerBounds[10] = 0
@@ -69,8 +69,16 @@ class Keras_CNN(Problem):
         lowerBounds[12] = 0.001
         upperBounds[12] = 1
 
+        # dropout rate 1
+        lowerBounds[13] = 0.1
+        upperBounds[13] = 0.9
+
+        # dropout rate 2
+        lowerBounds[14] = 0.1
+        upperBounds[14] = 0.9
+
         super(Keras_CNN, self).__init__(1,
-                                13,
+                                15,
                                 (lowerBounds, upperBounds))
         self.problem = "Keras_CNN"
 
@@ -114,8 +122,24 @@ class Keras_CNN(Problem):
             activation_full_1 = self.activations[int(round(params[i][10] * 10))]
             activation_full_2 = self.activations[int(round(params[i][11] * 10))]
             learning_rate = params[i][12]
+            dropout_1 = params[i][13]
+            dropout_2 = params[i][14]
 
-            print(kernel_size_1, kernel_size_2, activation_1, activation_2, pooling_1, pooling_2, n_kernels_1, n_kernels_2, neurons_1, neurons_2, activation_full_1, activation_full_2, learning_rate)
+            print("kernel size 1:", kernel_size_1)
+            print("kernel size 2:", kernel_size_2)
+            print("activation 1:", activation_1)
+            print("activation 2:", activation_2)
+            print("pooling 1:", pooling_1)
+            print("pooling 2:", pooling_2) 
+            print("n kernels 1:", n_kernels_1)
+            print("n kernels 2:", n_kernels_2)
+            print("neurons 1:", neurons_1)
+            print("neurons 2:", neurons_2)
+            print("activation full 1:", activation_full_1)
+            print("activation full 2:", activation_full_2) 
+            print("learning rate:", learning_rate)
+            print("dropout 1:", dropout_1)
+            print("dropout 2:", dropout_2)
 
             # build CNN
             model = Sequential()
@@ -131,8 +155,10 @@ class Keras_CNN(Problem):
                 model.add(AveragePooling2D())
             model.add(Flatten())
             model.add(Dense(neurons_1, activation=activation_full_1))
+            model.add(Dropout(dropout_1))
             model.add(Dense(neurons_2, activation=activation_full_2))
-            model.add(Dense(self.n_classes))
+            model.add(Dropout(dropout_2))
+            model.add(Dense(self.n_classes, activation='softmax'))
 
             optimizer = SGD(learning_rate=learning_rate)
             model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
@@ -149,9 +175,6 @@ class Keras_CNN(Problem):
             for j in range(len(self.y_test)):
                 test.append(np.argmax(self.y_test[j]))
 
-            print(accuracy_score(pred, test))
-            print(i)
-            print(all_means.shape)
             all_means[i][0] = -accuracy_score(pred, test)
         
         population.setNotEvaluatedObjectives(all_means)
