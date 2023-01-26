46e415bcf4d08fbc550081afc90f22301abc1342
deadlyshoes <khoseilmar@gmail.com>
2023-01-26 12:12:06 -0300

    adicionar score especificidade

---
+++ src/problems/scores.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def sensitivity_func(y_true, y_pred):
+    correct_positive_pred = y_pred[(y_true == 1)].sum()
+    total_positive = (y_true == 1).sum()
+    return correct_positive_pred / total_positive
+
+def specificity_func(y_true, y_pref):
+    correct_negative_pred = y_pred[(y_true == 0)].sum()
+    total_negative = (y_true == 0).sum()
+    return correct_positive_pred / total_positive    
