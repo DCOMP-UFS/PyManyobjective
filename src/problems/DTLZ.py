ba9b920a1553c9551c345978d1bfbb255c141e52
Eduardocesn <eduardocesn@academico.ufs.br>
2024-03-31 22:32:35 -0300

    Adicionado código do tdeadp

--- src/problems/DTLZ.py
+++ src/problems/DTLZ.py
@@ -10,6 +10,8 @@ from src.Solution import Solution
 from src.ParetoFront import ParetoFront
 import math
 import numpy as np
+from pymop.problem import Problem as Problem_pymop
+import autograd.numpy as anp
 
 # Problemas da classe DTLZ
 class DTLZ1(Problem):
@@ -153,3 +155,27 @@ class DTLZ7(DTLZ1):
         h = self.n_objs - np.sum(f / (1 + g[:, None]) * (1 + np.sin(3 * np.pi * f)), axis=1)
 
         population.objectives = np.column_stack([f, (1 + g) * h])
+
+# This is a modified DTLZ1 problem, according to the ParEGO paper
+# 20 * pi in the cosine term is replaced with 2 * pi to reduce the ruggedness of the function
+class DTLZ1a(Problem_pymop):
+    def __init__(self, n_var=6, n_obj=2):
+        self.k = n_var - n_obj + 1
+        super().__init__(n_var=n_var, n_obj=n_obj, n_constr=0, xl=0, xu=1, type_var=anp.double)
+
+    def g1(self, X_M):
+        return 100 * (self.k + anp.sum(anp.square(X_M - 0.5) - anp.cos(2 * anp.pi * (X_M - 0.5)), axis=1))
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        X_, X_M = x[:, :self.n_obj - 1], x[:, self.n_obj - 1:]
+        g = self.g1(X_M)
+
+        f = []
+        for i in range(0, self.n_obj):
+            _f = 0.5 * (1 + g)
+            _f *= anp.prod(X_[:, :X_.shape[1] - i], axis=1)
+            if i > 0:
+                _f *= 1 - X_[:, X_.shape[1] - i]
+            f.append(_f)
+
+        out["F"] = anp.column_stack(f)
